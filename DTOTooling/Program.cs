398b89707370cacabf4b91ad558073c7dc0aa2ba
Hani Ho <hani.herbert.ho@gmail.com>
2021-04-09 17:22:38 +0800

    feat: Created library Ho.DTOTooling.Lib to upload as NuGet package.

--- DTOTooling/Program.cs
+++ DTOTooling/Program.cs
@@ -2,6 +2,7 @@
 using Ho.FileHelper.Lib;
 using System.IO;
 using System.Text.RegularExpressions;
+using Ho.DTOTooling.Lib;
 
 namespace DTOTooling
 {
@@ -9,36 +10,9 @@ namespace DTOTooling
     {
         static void Main(string[] args)
         {
-            string patternToFindProperties = @"(public|private).*.+?(?=\{)";
-            string patternToFindMethods = @"(public|private).*.+?(?=\()";
-            Regex regex;
             string folderToParse = @"C:\Users\hanih\source\repos\DTOTooling\filesToParse";
-            DirectoryInfo d = new DirectoryInfo(folderToParse);
-            FileInfo[] Files = d.GetFiles("*.cs"); //Getting Text files
-            foreach (FileInfo file in Files)
-            {
-                string fileName = $"{Path.GetFileNameWithoutExtension(file.Name)}.txt";
-                FileHelper.SetOutputToFile("results", fileName, FileCreation.OverwriteFile);
-                string text = File.ReadAllText(file.FullName);
-                regex = new Regex(patternToFindProperties);
-                var matches = regex.Matches(text);
-                Console.WriteLine(file.Name);
-                Console.WriteLine("\nProperties:");
-                foreach (Match match in matches)
-                {
-                    Console.WriteLine($"\t{match.Value}");
-                }
-
-                regex = new Regex(patternToFindMethods);
-                matches = regex.Matches(text);
-                Console.WriteLine("\nMethods:");
-                foreach (Match match in matches)
-                {
-                    Console.WriteLine($"\t{match.Value}");
-                }
-
-                FileHelper.CloseOutputStream();
-            }
+            var myDTOTooling = new Ho.DTOTooling.Lib.DTOTooling(folderToParse, "results");
+            myDTOTooling.ListDownPropertiesAndMethods();
 
             return;
         }
---
+++ Ho.DTOTooling.Lib/DTOTooling.cs
@@ -0,0 +1,53 @@
+﻿using Ho.FileHelper.Lib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Ho.DTOTooling.Lib
+{
+    public class DTOTooling
+    {
+        private const string patternToFindProperties = @"(public|private).*.+?(?=\{)";
+        private const string patternToFindMethods = @"(public|private).*.+?(?=\()";
+        private string folderToParse;
+        private string resultsFolder;
+        private Regex regex;
+
+        public DTOTooling(string pathOfFolderToParse, string nameOfResultsFolder)
+        {
+            this.folderToParse = pathOfFolderToParse;
+            this.resultsFolder = nameOfResultsFolder;
+        }
+
+        public void ListDownPropertiesAndMethods()
+        {
+            DirectoryInfo d = new DirectoryInfo(folderToParse);
+            FileInfo[] Files = d.GetFiles(); foreach (FileInfo file in Files)
+            {
+                string fileName = $"{Path.GetFileNameWithoutExtension(file.Name)}.txt";
+                Ho.FileHelper.Lib.FileHelper.SetOutputToFile("results", fileName, FileCreation.OverwriteFile);
+                string text = File.ReadAllText(file.FullName);
+                regex = new Regex(patternToFindProperties);
+                var matches = regex.Matches(text);
+                Console.WriteLine(file.Name);
+                Console.WriteLine("\nProperties:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine($"\t{match.Value}");
+                }
+
+                regex = new Regex(patternToFindMethods);
+                matches = regex.Matches(text);
+                Console.WriteLine("\nMethods:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine($"\t{match.Value}");
+                }
+
+                Ho.FileHelper.Lib.FileHelper.CloseOutputStream();
+            }
+        }
+    }
+}
