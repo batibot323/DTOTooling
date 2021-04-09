eb01c00a98dfd2049900c1249bf22409c9cce6d4
Hani Ho <hani.herbert.ho@gmail.com>
2021-04-09 16:21:02 +0800

    Initial commit

---
+++ DTOTooling/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DTOTooling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
