78e02e99a12c1d223f894ceb690e355234dc9562
Hani Ho <hani.herbert.ho@gmail.com>
2021-04-09 17:01:21 +0800

    feat: Added listing down of all properties and methods of files inside  folder.

--- DTOTooling/Program.cs
+++ DTOTooling/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using Ho.FileHelper.Lib;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace DTOTooling
 {
@@ -6,7 +9,38 @@ namespace DTOTooling
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string patternToFindProperties = @"(public|private).*.+?(?=\{)";
+            string patternToFindMethods = @"(public|private).*.+?(?=\()";
+            Regex regex;
+            string folderToParse = @"C:\Users\hanih\source\repos\DTOTooling\filesToParse";
+            DirectoryInfo d = new DirectoryInfo(folderToParse);
+            FileInfo[] Files = d.GetFiles("*.cs"); //Getting Text files
+            foreach (FileInfo file in Files)
+            {
+                string fileName = $"{Path.GetFileNameWithoutExtension(file.Name)}.txt";
+                FileHelper.SetOutputToFile("results", fileName, FileCreation.OverwriteFile);
+                string text = File.ReadAllText(file.FullName);
+                regex = new Regex(patternToFindProperties);
+                var matches = regex.Matches(text);
+                Console.WriteLine(file.Name);
+                Console.WriteLine("\nProperties:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine($"\t{match.Value}");
+                }
+
+                regex = new Regex(patternToFindMethods);
+                matches = regex.Matches(text);
+                Console.WriteLine("\nMethods:");
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine($"\t{match.Value}");
+                }
+
+                FileHelper.CloseOutputStream();
+            }
+
+            return;
         }
     }
 }
